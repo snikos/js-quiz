59f5df99cc4c9de288d4c594e25e0a0b177d59f5
Niko <cobrascript@yandex.ru>
2021-06-06 17:48:32 +0800

    com1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Quiz from './Quiz';
+import QuizHook from './components/QuizHook';
+import { History } from './components/History';
+import { TodoList } from './components/TodoList';
+import { SelectFile } from './components/SelectFile';
+import logo from './cross.png';
 import './App.css';
 
-function App() {
+function App(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <nav className="App-header__navy">
+          <Link to="/">Quiz</Link>
+          <Link to="/history">History</Link>
+          <Link to="/todo">Todo</Link>
+          <Link to="/QuizHook">Quiz Hook</Link>
+        </nav>
+        <div>
+          <SelectFile />
+        </div>
       </header>
+      <div className="App-content">
+        <Route exact path="/" render={ (props) => <Quiz {...props} /> } />
+        <Route exact path="/history" render={ (props) => <History {...props} /> } />
+        <Route exact path="/todo" render={ (props) => <TodoList {...props} /> } />
+        <Route exact path="/QuizHook" render={ (props) => <QuizHook {...props} /> } />
+      </div>
     </div>
   );
 }
