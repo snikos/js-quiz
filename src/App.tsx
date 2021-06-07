1bc33a312f0d314bab18676898ad754bf3e0cc8f
Niko <cobrascript@yandex.ru>
2021-06-07 12:13:18 +0800

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //import React from 'react';
 import { Route, Link } from 'react-router-dom';
-import Quiz from './Quiz';
+//import Quiz from './Quiz';
+import Dani from './components/Dani';
 import QuizHook from './components/QuizHook';
 import { History } from './components/History';
 import { TodoList } from './components/TodoList';
@@ -16,20 +17,20 @@ function App(){
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <nav className="App-header__navy">
-          <Link to="/js-quiz/Quiz">Quiz</Link>
+          <Link to="/js-quiz/">Quiz javascript</Link>
           <Link to="/js-quiz/history">History</Link>
           <Link to="/js-quiz/todo">Todo</Link>
-          <Link to="/js-quiz/QuizHook">Quiz Hook</Link>
+          <Link to="/js-quiz/Dani">Dani</Link>
         </nav>
         <div>
           <SelectFile />
         </div>
       </header>
       <div className="App-content">
-        <Route exact path="/js-quiz/Quiz" render={ (props) => <Quiz {...props} /> } />
+        <Route exact path="/js-quiz/" render={ (props) => <QuizHook {...props} /> } />
         <Route exact path="/js-quiz/history" render={ (props) => <History {...props} /> } />
         <Route exact path="/js-quiz/todo" render={ (props) => <TodoList {...props} /> } />
-        <Route exact path="/js-quiz/QuizHook" render={ (props) => <QuizHook {...props} /> } />
+        <Route exact path="/js-quiz/Dani" render={ (props) => <Dani {...props} /> } />
       </div>
     </div>
   );
